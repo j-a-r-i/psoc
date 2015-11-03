d331151686823db1982520d8c2686c79199806f2
Jari Ojanen <jari.ojanen@espotel.com>
2015-11-03 15:50:00 +0200

    added some components

--- Design01/Design01.cydsn/main.c
+++ Design01/Design01.cydsn/main.c
@@ -11,11 +11,35 @@
 */
 #include <project.h>
 
+CY_ISR_PROTO(Timer1ISR);
+CY_ISR_PROTO(Spi1ISR);
+
+CY_ISR(Timer1ISR)
+{
+}
+
+CY_ISR(Spi1ISR)
+{
+
+}
+
+
+
 int main()
 {
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
-    /* CyGlobalIntEnable; */ /* Uncomment this line to enable global interrupts. */
+    spi_Init();
+    timer1_Init();
+    CyBle_Init();
+    
+    // Start interrupts
+    //
+    isrTimer1_StartEx(Timer1ISR);
+    spi_SCB_IRQ_StartEx(Spi1ISR);
+    
+    CyGlobalIntEnable; /* Uncomment this line to enable global interrupts. */
+    
     for(;;)
     {
         /* Place your application code here. */
