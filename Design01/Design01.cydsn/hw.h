cd1db1f011fd534e2250a36a73e916f731657e2e
Jari Ojanen <jari.ojanen@espotel.com>
2015-11-06 14:39:33 +0200

    uses DS1820

---
+++ Design01/Design01.cydsn/hw.h
@@ -0,0 +1,29 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#ifndef _HW_H
+#define _HW_H
+
+#include <stdint.h>
+
+extern void config_port_set (uint8_t pin);
+extern void config_port_clear (uint8_t pin);
+extern void config_port_mode (uint8_t pin, uint8_t out);
+extern uint8_t config_port_read (uint8_t pin);
+
+
+extern void delay_us(uint16_t usDelay);
+extern void delay_ms(uint16_t msDelay);
+
+#define PIN_OW1 0
+
+#endif
+/* [] END OF FILE */
